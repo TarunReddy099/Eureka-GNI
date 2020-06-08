eb1b357a30a14a9984492721ee30ed2559b4aaed
TarunReddy099 <tarunreddy099@gmail.com>
2020-06-08 13:12:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/eureka_gni/ui/home/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.example.eureka_gni.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.eureka_gni.R
+
+class HomeFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View = inflater!!.inflate(R.layout.fragment_home, container, false)
+
+
+
+        return view
+        return root
+
+    }
+}
---
+++ app/src/main/java/com/example/eureka_gni/ui/slideshow/SlideshowFragment.kt
@@ -0,0 +1,32 @@
+package com.example.eureka_gni.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.eureka_gni.R
+
+class SlideshowFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        val view: View = inflater!!.inflate(R.layout.fragment_slideshow, container, false)
+
+       // val e1=view.findViewById<Button>(R.id.r1)
+
+        return view
+        return root
+
+    }
+}
